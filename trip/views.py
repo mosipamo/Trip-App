cef0871d3f17f771255dc19d3dae724bae94e51f
sina <sina.sipamo@gmail.com>
2025-08-07 13:06:02 +0330

    Added Trips via Admin and Created View to List Out Trips

--- trip/views.py
+++ trip/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Trip, Note
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = "trip/index.html"
     
+
+def trips_list(request):
+    trips = Trip.objects.all()
+    context = {
+        "trips": trips
+    }
+    
+    return render(request=request, template_name="trip/trips_list.html", context=context)
