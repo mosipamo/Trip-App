4de6c1523db47bdcf66ad9522f314c2507b8e43c
sina <sina.sipamo@gmail.com>
2025-08-07 14:10:58 +0330

    Added login template page

--- config/settings.py
+++ config/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/' # www.mysite.com/media/png-1
 MEDIA_ROOT = BASE_DIR / 'media/' # documents/file-xyz.png
+
+# where should the user go after login - if not next
+LOGIN_REDIRECT_URL = "trip-list"
