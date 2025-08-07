a3439e39704e23d86eefcf1eda38a832b0e3d7df
sina <sina.sipamo@gmail.com>
2025-08-07 17:46:11 +0330

    Created logout page and checked if user is logged in or not

--- trip/urls.py
+++ trip/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import HomeView, trips_list
+from .views import HomeView, trip_list
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
-    path('dashboard/', trips_list, name="trip-list"),
+    path('dashboard/', trip_list, name="trip-list"),
 ]
+
+"""
+    accounts/login/ [name='login']
+    accounts/logout/ [name='logout']
+    accounts/password_change/ [name='password_change']
+    accounts/password_change/done/ [name='password_change_done']
+    accounts/password_reset/ [name='password_reset']
+    accounts/password_reset/done/ [name='password_reset_done']
+    accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+    accounts/reset/done/ [name='password_reset_complete']
+"""
--- trip/views.py
+++ trip/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import Trip, Note
+from django.contrib.auth import logout
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = "trip/index.html"
     
 
-def trips_list(request):
+def trip_list(request):
     trips = Trip.objects.all()
     context = {
         "trips": trips
     }
     
-    return render(request=request, template_name="trip/trips_list.html", context=context)
+    return render(request=request, template_name="trip/trip_list.html", context=context)
